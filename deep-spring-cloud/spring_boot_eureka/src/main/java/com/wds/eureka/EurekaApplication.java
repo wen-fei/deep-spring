42d0c74b1dfedc35f432a2d3b71a47d55b144359
wen-fei <282699766@qq.com>
2019-06-18 23:18:37 +0800

    spring cloud eureka

---
+++ deep-spring-cloud/spring_boot_eureka/src/main/java/com/wds/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.wds.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : TenYun
+ * @date : 2019-06-18 23:13
+ * @description :
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
