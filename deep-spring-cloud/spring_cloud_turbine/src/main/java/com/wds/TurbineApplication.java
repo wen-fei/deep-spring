0c3a5154f9d97bfc8254691e99cadc727141e501
wen-fei <282699766@qq.com>
2019-06-20 22:21:24 +0800

    spring cloud turbine

---
+++ deep-spring-cloud/spring_cloud_turbine/src/main/java/com/wds/TurbineApplication.java
@@ -0,0 +1,21 @@
+package com.wds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author : TenYun
+ * @date : 2019-06-20 10:47
+ * @description :
+ **/
+@EnableTurbine
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+}
