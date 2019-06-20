2ee41a457ced960316d1523e0e72087a867acd15
wen-fei <282699766@qq.com>
2019-06-20 22:18:30 +0800

    spring cloud distributed eureka server  port must be 8761

---
+++ deep-spring-cloud/spring_cloud_eureka_server/src/main/java/com/wds/sureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.wds.sureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : TenYun
+ * @date : 2019-06-19 10:32
+ * @description :
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
