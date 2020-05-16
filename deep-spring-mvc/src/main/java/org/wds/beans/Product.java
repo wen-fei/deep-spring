87444a47d605b6f5a081ba0b1656c27676d36104
wen-fei <282699766@qq.com>
2020-05-16 16:43:57 +0800

    test bean: Product

---
+++ deep-spring-mvc/src/main/java/org/wds/beans/Product.java
@@ -0,0 +1,18 @@
+package org.wds.beans;
+
+/**
+ * @author : TenYun
+ * @date : 2020-05-16 16:26
+ * @description :
+ **/
+public class Product {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
