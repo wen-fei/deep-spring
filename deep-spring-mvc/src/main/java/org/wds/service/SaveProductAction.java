20bd4e08d903521437aa72eca587038241a990f0
wen-fei <282699766@qq.com>
2020-05-16 23:53:07 +0800

    service

---
+++ deep-spring-mvc/src/main/java/org/wds/service/SaveProductAction.java
@@ -0,0 +1,15 @@
+package org.wds.service;
+
+import org.wds.beans.Product;
+
+/**
+ * @author : TenYun
+ * @date : 2020-05-16 23:52
+ * @description :
+ **/
+public class SaveProductAction {
+
+    public void save(Product product) {
+        // insert product into database
+    }
+}
