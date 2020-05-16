bd65410899e46d5b429ef942cf2f7486b52a606a
wen-fei <282699766@qq.com>
2020-05-16 16:56:37 +0800

    Product pojo

--- deep-spring-mvc/src/main/java/org/wds/beans/Product.java
+++ deep-spring-mvc/src/main/java/org/wds/beans/Product.java
@@ -1,12 +1,26 @@
 package org.wds.beans;
 
+import java.io.Serializable;
+
 /**
  * @author : TenYun
  * @date : 2020-05-16 16:26
  * @description :
  **/
-public class Product {
+public class Product  implements Serializable {
+    private static final long serialVersionUID = 748392348L;
     private String name;
+    private String description;
+    private float price;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, float price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -14,5 +28,21 @@ public class Product {
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 }
