505005f365cd4b94e027728f122e19267417ba94
wen-fei <282699766@qq.com>
2020-05-30 16:56:31 +0800

    add pojo: Quote

---
+++ spring-boot-demo/springboot-RESTful/src/main/java/org/wds/pojo/Quote.java
@@ -0,0 +1,39 @@
+package org.wds.pojo;
+
+/**
+ * @author : TenYun
+ * @date : 2020-05-30 16:46
+ * @description :
+ **/
+public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
