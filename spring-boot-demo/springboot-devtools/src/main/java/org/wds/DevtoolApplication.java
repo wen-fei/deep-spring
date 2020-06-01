970feb110504da4ca637bb75b6f3115438327e20
wen-fei <282699766@qq.com>
2020-06-01 23:09:13 +0800

    add func: application.yml 排除不想触发重启的文件目录

---
+++ spring-boot-demo/springboot-devtools/src/main/java/org/wds/DevtoolApplication.java
@@ -0,0 +1,17 @@
+package org.wds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : TenYun
+ * @date : 2020-06-01 8:07
+ * @description :
+ **/
+@SpringBootApplication
+public class DevtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtoolApplication.class, args);
+    }
+}
