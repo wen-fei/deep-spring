f7bf254b0ed598eb9876e310e649adc377a4ff48
wen-fei <282699766@qq.com>
2020-06-08 23:45:12 +0800

    add module: spring mybatis

---
+++ deep-spring-boot/spring_boot_mybatis/src/main/java/org/wds/MybatisApplication.java
@@ -0,0 +1,16 @@
+package org.wds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : TenYun
+ * @date : 2019-08-19 11:00
+ * @description :
+ **/
+@SpringBootApplication
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
---
+++ deep-spring-boot/spring_boot_mybatis/src/main/java/org/wds/datasource.java
@@ -0,0 +1,9 @@
+package org.wds;
+
+/**
+ * @author : TenYun
+ * @date : 2019-08-19 11:00
+ * @description :
+ **/
+public class datasource {
+}
